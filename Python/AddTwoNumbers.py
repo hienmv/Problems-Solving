501b1e0975b8a7c25ebaa55bef5a0efa1bff4dea
Hien Mv <hienmv.vn@gmail.com>
2018-05-27 23:50:35 +0900

    Add Two Number with singly-linked list

---
+++ Python/AddTwoNumbers.py
@@ -0,0 +1,50 @@
+# Definition for singly-linked list.
+
+
+"""
+ Add Two Numbers - https://leetcode.com/problems/add-two-numbers/description/
+
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+"""
+
+class listnode:
+    def __init__(self, x):
+        self.val = x
+        self.next = none
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        dummyHead = ListNode(0)
+        iterNode = dummyHead
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry +=l2.val
+                l2 = l2.next
+            
+            iterNode.next = ListNode(carry % 10)
+            iterNode = iterNode.next
+            
+            carry //= 10
+            
+        return dummyHead.next    
+
+        
