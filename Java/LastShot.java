76ce8eadae6a2b196826cfb022cefed99a010825
Hien Mv <hienmv.vn@gmail.com>
2019-04-22 00:11:49 +0900

    Last Short wrong answer

---
+++ Java/LastShot.java
@@ -0,0 +1,53 @@
+/** https://www.spoj.com/problems/LASTSHOT/
+ * idea: dfs
+ */
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+
+class LastShot {
+    static int getImpact(int vertex, ArrayList<ArrayList<Integer>> graph, boolean[] visitedArr) {
+        int count = 0;
+        Deque<Integer> queue = new LinkedList<>();
+        visitedArr[vertex] = true;
+        queue.addLast(vertex);
+        while(!queue.isEmpty()) {
+            count++;
+            int node = queue.pollLast();
+            for (int childNode : graph.get(node)) {
+                if (!visitedArr[childNode]) {
+                    visitedArr[childNode] = true;
+                    queue.addLast(childNode);
+                }
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>(n+1);
+        for (int i=0; i < n+1; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        for (int i=0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+        }
+        boolean[] visitedArr = new boolean[n+1];
+        int maxImpact = 1;
+        for (int node=1; node < graph.size(); node++) {
+            if (!visitedArr[node]) {
+                int count = getImpact(node, graph, visitedArr);
+                if (count > maxImpact) {
+                    maxImpact = count;
+                }
+            }
+        }
+        System.out.println(maxImpact);
+    }
+}
