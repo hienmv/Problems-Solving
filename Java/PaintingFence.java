d7d55c6fe7cbe9eb80c4b1fc829107f457214485
Hien Mv <hienmv.vn@gmail.com>
2019-09-23 23:56:32 +0900

    Divide and Conquer

--- Java/PaintingFence.java
+++ Java/PaintingFence.java
@@ -1,35 +1,73 @@
 /**
- * idea: divide and conquer //doing
+ * idea: divide and conquer
+ [L, R]
+
+ - Tô dọc (R - L + 1)
+ - Tô ngang
+  + tim min
+  + giam h[i] - min
+  + cost: min
+  + D&C
+ 
+ 50 5 5 6 3 
+ 
+ n = 5
+    -
+  -     -
+- |
+  |   -
+  |   |
+---------
+---------------------
+5 6 7 3 6
+
+2 3 4 0 3
  */
 
 import java.util.Scanner;
+import java.util.HashSet;
 
 class PaintingFence {
-    static int MAX_STROKES = 5001;
-    static int bruteForce(int[] arr, int left, int right) {
-        int minStrokes = MAX_STROKES;
+    static long getMinimumStrokes(int[] arr, int left, int right) {
         int minHeight = Integer.MAX_VALUE;
+        for(int i=left; i < right; i++) {
+            if (arr[i] < minHeight) {
+                minHeight = arr[i];
+            }
+        }
+        if (minHeight == Integer.MAX_VALUE) {
+            return 0;
+        }
+        
+        // reduce Height
+        long strokeNum = minHeight;
+        HashSet<Integer> breakIndexSet = new HashSet<>();
         for (int i=left; i < right; i++) {
-            minHeight = Math.min(minHeight, arr[i]);
+            arr[i] -= minHeight;
+            if (arr[i] == 0) {
+                breakIndexSet.add(i);
+            }
         }
-
-    }
-    static int getMinimumStrokes(int[] arr, int left, int right) {
-        if (right - left <= 2) {
-
+        int l = left, r = right;
+        for (int breakIndex : breakIndexSet) {
+            strokeNum += getMinimumStrokes(arr, l, breakIndex);
+            l = breakIndex + 1;
         }
+        strokeNum += getMinimumStrokes(arr, l, r);
+
+        return Math.min(strokeNum, right - left);
     }
 
     public static void main(String[] args) {
+        // input
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i=0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
-        int maxStrokes = n;
-        int numStrokes = getMinimumStrokes(arr, 0, n);
-        System.out.println(Math.min(maxStrokes, numStrokes));
+        // calculate
+        long numStrokes = getMinimumStrokes(arr, 0, n);
+        System.out.println(numStrokes);
     }
 }
