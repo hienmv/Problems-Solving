1d2efd6d7c15dd960ab2705ec55a701bbe44d2c0
Hien Mv <hienmv.vn@gmail.com>
2019-11-25 22:10:30 +0900

    refactor

--- Java/Alphacode.java
+++ Java/Alphacode.java
@@ -1,7 +1,7 @@
 /**
  * #dp
  */
-
+/*
 import java.util.Scanner;
 import java.util.ArrayList;
 class Alphacode {
@@ -34,6 +34,35 @@ class Alphacode {
                 }
                 else {
                     arr[i+1] = arr[i];
+                }
+            }
+            System.out.println(arr[sz]);
+        }
+    }
+}
+*/
+// better way
+import java.util.Scanner;
+class Alphacode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            String s = sc.next();
+            if (s.equals("0")) break;
+            int sz = s.length();
+            long[] arr = new long[sz + 1];
+            arr[0] = 1;
+            arr[1] = 1;
+            int num;
+            for (int i=1; i < sz; i++) {
+                num = Integer.parseInt(s.substring(i-1, i+1));
+                // 1-digits
+                if (s.charAt(i) != '0') {
+                    arr[i+1] += arr[i];
+                }
+                // 2-digits
+                if (num > 9 && num < 27) {
+                    arr[i + 1] += arr[i - 1];
                 }
             }
             System.out.println(arr[sz]);
